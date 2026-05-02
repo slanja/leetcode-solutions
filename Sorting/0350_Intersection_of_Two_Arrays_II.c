115896e37c94c5e0db7838c1d3faf91c3e85a7e3
Jan Slaný <slany3.jan@gmail.com>
2026-05-02 10:51:27 +0200

    350. Intersection of Two Arrays II using two pointers

--- Sorting/0350_Intersection_of_Two_Arrays_II.c
+++ Sorting/0350_Intersection_of_Two_Arrays_II.c
@@ -1,32 +1,41 @@
+int comp(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
 int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
+    qsort(nums1, nums1Size, sizeof(int), comp);
+    qsort(nums2, nums2Size, sizeof(int), comp);
+    
     // getting max size of result array
     int maxSize = (nums1Size < nums2Size) ? nums1Size : nums2Size;
     int* result = (int*)malloc(maxSize * sizeof(int));
 
     size_t pos = 0;
+    size_t i = 0;
+    size_t j = 0;
 
-    // going through every element and checking if they match
-    for (size_t i = 0; i < nums1Size; i++) {
-        for (size_t j = 0; j < nums2Size; j++) {
-            // if they match, add the number to the results
-            if (nums1[i] == nums2[j]) {
-                result[pos] = nums1[i];
-                // works here but not ideal for real situation :)
-                nums2[j] = -1;
-                pos++;
-
-                // check if the results are full, if they are return result
-                if (pos == maxSize) {
-                    *returnSize = pos;
-                    return result;
-                }
-
-                break;
+    while (i < nums1Size && j < nums2Size) {
+        // if they match, add the number to the results
+        if (nums1[i] == nums2[j]) {
+            // check if the results are full, if so, return the result
+            if (pos == maxSize) {
+                *returnSize = pos;
+                return result;
             }
-        }
-    }
-    
-    *returnSize = pos;
 
+            // write down the result of the comparison and increment all iterators
+            result[pos] = nums1[i];
+            pos++; i++; j++;
+        }
+
+        else if (nums1[i] < nums2[j]) {
+            i++;
+        }
+
+        else if (nums1[i] > nums2[j]) {
+            j++;
+        }        
+    }
+    *returnSize = pos;
     return result;
 }
