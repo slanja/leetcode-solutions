11307f01dcd27bdc6cab752e61b0ef66b632d07d
Jan Slaný <slany3.jan@gmail.com>
2026-05-04 22:18:16 +0200

    improved 509. Fibonacci Number

--- Math/0509_Fibonacci_Number.c
+++ Math/0509_Fibonacci_Number.c
@@ -1,5 +1,12 @@
 int fib(int n){
-    if (n < 2) return n;
+    unsigned fib_n2 = 0;
+    unsigned fib_n1 = 1;
 
-    return fib(n-1) + fib(n-2);
+    for (size_t i = 0; i < n; i++) {
+        unsigned tmp = fib_n2 + fib_n1;
+        fib_n2 = fib_n1;
+        fib_n1 = tmp;
+    }
+
+    return fib_n2;
 }
