a50a379081aac78ef59ed3b82f77acbbd1d9859c
Jan Slaný <slany3.jan@gmail.com>
2026-05-14 22:18:07 +0200

    added comments

--- Array/0136_Single_Number.c
+++ Array/0136_Single_Number.c
@@ -8,10 +8,12 @@ int singleNumber(int* nums, int numsSize) {
     size_t i = 0;
 
     while (i < numsSize-1) {
+        // If the current element is the same as the next one, skip both
         if (nums[i] == nums[i+1]) i += 2;
 
+        // If not, return the current element as it is the single number
         else return nums[i];
     }
-
+    // If we exit the loop, the single number is the last element in the array
     return nums[numsSize-1];
 }
