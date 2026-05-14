ed6fcacc69a64d595dbe7882138ba8e837e9752b
Jan Slaný <slany3.jan@gmail.com>
2026-05-14 22:19:44 +0200

    added comments

--- Linked_List/0206_Reverse_Linked_List.c
+++ Linked_List/0206_Reverse_Linked_List.c
@@ -4,10 +4,10 @@ struct ListNode* reverseList(struct ListNode* head) {
     struct ListNode *next = NULL;
 
     while (curr != NULL) {
-        next = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = next;
+        next = curr->next; // Store the next node before changing the current node's next pointer
+        curr->next = prev; // Reverse the current node's pointer to point to the previous node
+        prev = curr;       // Move the previous pointer to the current node
+        curr = next;       // Move to the next node in the original list (which is stored in next)
     }
     return prev;
 }
