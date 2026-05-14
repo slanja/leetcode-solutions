2c4c5a5930bc0d67d339a517684dd528fe060a1f
Jan Slaný <slany3.jan@gmail.com>
2026-05-14 22:11:28 +0200

    added comments and simplified the code

--- Array/2784_Check_if_Array_is_Good.c
+++ Array/2784_Check_if_Array_is_Good.c
@@ -3,21 +3,23 @@ int comp(const void *a, const void *b) {
 }
 
 bool isGood(int* nums, int numsSize) {
+    // sort the array in ascending order using qsort
     qsort(nums, numsSize, sizeof(int), comp);
 
-    size_t i = 0;
-    int prev = 0;
+    size_t i = 0; // index to iterate through the sorted array
+    int prev = 0; // variable to keep track of the previous element in the array
 
+    // if the array has only one element, it cannot be good
     if (numsSize == 1) return false;
 
     while (i < numsSize) {
+        // checking if the last two elements are the same and equal to n - 1
         if (i >= numsSize - 2 && nums[i] == numsSize - 1) i++;
 
-        else if (nums[i] == prev + 1) {
-            prev = nums[i];
-            i++;
-        }
+        // checking if the current element is equal to the previous element + 1
+        else if (nums[i] == prev + 1) prev = nums[i++];
 
+        // if not return false
         else return false;
     }
     return true;
