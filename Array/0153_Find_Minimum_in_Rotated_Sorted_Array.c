0b56cea06415748fd0ab685122e94110d58d63b7
Jan Slaný <slany3.jan@gmail.com>
2026-05-15 22:49:13 +0200

    added comments

--- Array/0153_Find_Minimum_in_Rotated_Sorted_Array.c
+++ Array/0153_Find_Minimum_in_Rotated_Sorted_Array.c
@@ -3,7 +3,9 @@ int comp(const void *a, const void *b) {
 }
 
 int findMin(int* nums, int numsSize) {
+    // sort the array in ascending order using qsort
     qsort(nums, numsSize, sizeof(int), comp);
 
+    // return the first element of the sorted array, which is the minimum element
     return nums[0];
 }
