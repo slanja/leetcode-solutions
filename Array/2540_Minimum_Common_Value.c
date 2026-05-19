78fd85fd6d882de7c13b9cce4891409dd5b65d28
Jan Slaný <slany3.jan@gmail.com>
2026-05-19 22:50:53 +0200

    simplified whole code, removed useless variable min and remade all if statements

--- Array/2540_Minimum_Common_Value.c
+++ Array/2540_Minimum_Common_Value.c
@@ -2,21 +2,14 @@ int getCommon(int* nums1, int nums1Size, int* nums2, int nums2Size) {
     int i = 0;
     int j = 0;
 
-    // Initialize min to the first element of nums1
-    int min = nums1[0];
-
     while (i < nums1Size && j < nums2Size) {
         // If nums2[j] is equal to min, return min as the minimum common value
-        if (nums2[j] == min) return min;
+        if (nums1[i] == nums2[j]) return nums1[i];
 
         // If nums2[j] is less than min, move the pointer j to the right
-        if (nums2[j] < min) j++;
+        else if (nums1[i] < nums2[j]) i++;
 
-        else {
-            ++i;
-            // If i is out of bounds, set min to a value that is greater than any element in nums1
-            min = i < nums1Size ? nums1[i] : nums1[nums1Size];
-        }
+        else j++;
     }
     return -1;
 }
