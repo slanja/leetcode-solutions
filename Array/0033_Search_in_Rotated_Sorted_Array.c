8901c167a00863f0a9182114348f32bf60621fa5
slany3.jan <97552526+slanja@users.noreply.github.com>
2026-05-22 22:12:19 +0200

    added 33. Search in Rotated Sorted Array

---
+++ Array/0033_Search_in_Rotated_Sorted_Array.c
@@ -0,0 +1,10 @@
+int search(int* nums, int numsSize, int target) {
+    int i = 0;
+    
+    while (i < numsSize) {
+        if (nums[i] == target) return i;
+        
+        i++;
+    }
+    return -1;
+}
