155a9b51dc56e521a466ad3269cd2f4aa56fe25f
Jan Slaný <slany3.jan@gmail.com>
2026-05-23 10:34:47 +0200

    added comments

--- Array/0033_Search_in_Rotated_Sorted_Array.c
+++ Array/0033_Search_in_Rotated_Sorted_Array.c
@@ -2,6 +2,7 @@ int search(int* nums, int numsSize, int target) {
     int i = 0;
     
     while (i < numsSize) {
+        // If nums[i] is equal to target, return i as the index of the target
         if (nums[i] == target) return i;
         
         i++;
