9a5675638a3083c398d1418c4ab060ad2cfd9616
Jan Slaný <slany3.jan@gmail.com>
2026-05-25 22:18:21 +0200

    added 396. Rotate Function

--- Math/0396_Rotate_Function.c
+++ Math/0396_Rotate_Function.c
@@ -7,14 +7,14 @@ int maxRotateFunction(int* nums, int numsSize) {
     // Calculate the sum of the array and the initial F(0)
     for (int i = 0; i < numsSize; i++) {
         sum += nums[i];
-        currentF += (long long)i * nums[i];
+        currentF += i * nums[i];
     }
 
     long long maxF = currentF;
 
     // Calculate F(k) for k = 1 to n-1 using the relation F(k) = F(k-1) + sum - n * nums[n-k]
     for (int i = 1; i < numsSize; i++) {
-        currentF = currentF + sum - (long long)numsSize * nums[numsSize - i];
+        currentF = currentF + sum - numsSize * nums[numsSize - i];
 
         if (currentF > maxF) maxF = currentF;
     }
